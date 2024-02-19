24a3db7d5bf446d3b9bfb9e7a939b96d929bedfa
Andrea Furtunato <andreafps.dev@gmail.com>
2024-02-19 16:57:11 -0300

    criação do login e cadastro

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,28 +1,31 @@
-import homeLogo from '../../assets/astronauta (2).png'
+import React from 'react';
+import homeLogo from '../../assets/astronauta (3).png'
+import './Home.css';
 
 function Home() {
-  return (
-    <>
-      <div className="bg-indigo-900 flex justify-center">
-        <div className="container grid grid-cols-2 text-white">
-          <div className="flex flex-col gap-4 items-center justify-center py-4">
-            <h2 className="text-5xl font-bold">Seja bem vindo!</h2>
-            <p className="text-xl">Expresse suas opiniões aqui com a gente!</p>
-
-            <div className="flex justify-around gap-4">
-              <button className="rounded bg-white text-blue-800 py-2 px-4">
-                Ver postagens
-              </button>
+    return (
+        <>
+        <div className="bg-indigo-900 flex justify-center">
+          <div className='container grid grid-cols-2 text-white'>
+            <div className="flex flex-col gap-4 items-center justify-center py-4">
+              <h2 className='text-5xl font-bold'>Seja bem vindo!</h2>
+              <p className='text-xl'>Desperte a sua imaginação e faça uma viagem cósmica com suas ideias e opiniões!</p>
+  
+              <div className="flex justify-around gap-4">
+              
+                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+              </div>
+            </div>
+  
+            <div className="flex justify-center ">
+              <img src={homeLogo} alt="" className='w-2/3' />
+      
             </div>
           </div>
-
-          <div className="flex justify-center ">
-            <img src={homeLogo} alt="" className="w-2/3" />
-          </div>
         </div>
-      </div>
-    </>
-  )
+        
+      </>
+    );
 }
 
-export default Home
+export default Home;
